128db8a62760e910aeb908e5576897959e2f9f01
Sebastian Caccavallo <electgpl@gmail.com>
2018-03-16 14:13:41 -0300

    Add files via upload

---
+++ Desarrollo_V1.0/PLCArduinoV1.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"pvengine.exe" +W320 +H240 +FN +L"c:/windows/fonts" +L"C:/EAGLE 8.2.0 Portable/EAGLE 8.2.0/ulp//../povray" +O"C:/Users/Electgpl/Desktop/PLCArduinoV1.png" /RENDER "C:/Users/Electgpl/Desktop/PLCArduinoV1.pov" 
