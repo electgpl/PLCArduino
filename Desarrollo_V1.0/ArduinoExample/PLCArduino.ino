40f2b869bd3b25d980e15ec953ab0baa2b0e2478
Sebastian Caccavallo <electgpl@gmail.com>
2018-04-11 20:59:27 -0300

    Add files via upload

---
+++ Desarrollo_V1.0/ArduinoExample/PLCArduino.ino
@@ -0,0 +1,33 @@
+#include "PLC.h"
+
+void setup() {
+  
+   Serial.begin(9600);
+   PLC_Setup();
+   
+}
+
+void loop() {
+  
+   for( int i=0; i<100; i++) {
+    
+      if( !PLC_ReadContact(DIGITAL_IN_1) ) {
+         PLC_WriteRelay(DIGITAL_OUT_1, true);
+         PLC_WriteRelay(DIGITAL_OUT_3, false);
+      }
+      
+      if( !PLC_ReadContact(DIGITAL_IN_3) ){
+         PLC_WriteRelay(DIGITAL_OUT_1, false);
+         PLC_WriteRelay(DIGITAL_OUT_3, true);
+      }
+      
+      delay(10);
+   }
+   
+   Serial.print("Canal Analogo 2: ");
+   Serial.println( PLC_ReadAnalogInput(ANALOG_IN_2) );
+   
+   if( PLC_ReadAnalogInput(ANALOG_IN_2) > 6.0 ) {
+      Serial.println("Alarma de sobre tension mayor a 6V");
+   }
+}
